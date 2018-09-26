a78eef0214c3dac4c21fdb729ba848e79c7c3c29
Andres Duque <saduqz@gmail.com>
2018-09-26 11:12:50 -0500

    Initial commit

---
+++ realtime_chat/database_info.py
@@ -0,0 +1,29 @@
+ENGINE = "django.contrib.gis.db.backends.postgis"
+
+# Local DB
+# NAME = "liftit_backup_1"
+# HOST = "localhost"
+# PORT = "5432"
+# USER = "postgres"
+# PASSWORD = ""
+
+# QA
+HOST = "qa-aa1l4nr8mqmj403.cv4fqsjhumdd.us-east-1.rds.amazonaws.com"
+NAME = "ebdb"
+PORT = "5432"
+USER = "liftitDevUsers"
+PASSWORD = "liftitDevUsers"
+
+# Sandbox
+# NAME = "ebdb"
+# HOST = "aa1l4nr8mqmj403.cv4fqsjhumdd.us-east-1.rds.amazonaws.com"
+# PORT = "5432"
+# USER = "liftitDevUsers"
+# PASSWORD = "liftitDevUsers"
+
+# Production
+# NAME = "ebdb"
+# HOST = "aaq9zpa9a2wo8c.cv4fqsjhumdd.us-east-1.rds.amazonaws.com"
+# PORT = "5432"
+# USER = "liftitDevUsers"
+# PASSWORD = "liftitDevUsers"
